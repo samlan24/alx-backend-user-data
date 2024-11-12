a1768982e96473cc957ad504c3dcb21c945d6f10
samlan24 <muchemionline@gmial.com>
2024-11-12 13:34:20 +0000

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """basic auth class"""
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -9,7 +10,7 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
-        """base64 authorization header"""
+        """Extracts the Base64 part of the Authorization header"""
         if authorization_header is None:
             return None
         if type(authorization_header) is not str:
@@ -17,3 +18,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[len('Basic '):]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes the Base64 authorization header"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
