265c0e95f91acfbda90c20888534863cdb225c35
samlan24 <muchemionline@gmial.com>
2024-11-12 12:07:37 +0000

    auth methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,25 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth method
         """
-        return False
+        if path is None:
+            return True
+        
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        
+        if path in excluded_paths:
+            return False
+        
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization_header method
