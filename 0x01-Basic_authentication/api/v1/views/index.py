eda4ea1a68b83c2038a58f9b6cd78f89f77f59f5
samlan24 <muchemionline@gmial.com>
2024-11-12 11:25:16 +0000

    adding custom error pages

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """return abort 401"""
+    return abort(401)
