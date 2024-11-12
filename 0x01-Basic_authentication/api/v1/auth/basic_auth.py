3f28af777b2cc7d57c61997c3e02311cd01f42af
samlan24 <muchemionline@gmial.com>
2024-11-12 16:29:09 +0000

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,15 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """Extracts the user credentials from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if type(decoded_base64_authorization_header) is not str:
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        user_credentials = decoded_base64_authorization_header.split(':', 1)
+        return (user_credentials[0], user_credentials[1])
